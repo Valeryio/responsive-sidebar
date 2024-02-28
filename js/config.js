9edb3dbb0268012133ab87f066da030ce46eb0b8
Valeryio <tommagoma23@gmail.com>
2024-02-28 21:47:29 +0100

    Adding some javascript variables

---
+++ js/config.js
@@ -0,0 +1,18 @@
+/**
+ * Declaring some variables to have some html elements
+ */
+
+let brandLogo = document.querySelector(".brand-logo");
+console.log(brandLogo);
+
+let amburgerMenu = document.querySelector(".amburger-menu");
+console.log(amburgerMenu);
+
+let body = document.querySelector("body");
+console.log(body);
+
+let navbarHeader = document.querySelector(".navbar-header");
+console.log(navbarHeader);
+
+let navbarFooter = document.querySelector(".navbar-footer");
+console.log(navbarFooter);
