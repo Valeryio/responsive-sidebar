05b695e4c363c1f9a8cbdd533ee957193f177577
Valeryio <tommagoma23@gmail.com>
2024-03-01 11:38:22 +0100

    Cleaning the CSS CODE

--- js/config.js
+++ js/config.js
@@ -2,17 +2,40 @@
  * Declaring some variables to have some html elements
  */
 
+/*
 let brandLogo = document.querySelector(".brand-logo");
 console.log(brandLogo);
 
 let amburgerMenu = document.querySelector(".amburger-menu");
-console.log(amburgerMenu);
+console.log(amburgerMenu);*/
 
 let body = document.querySelector("body");
 console.log(body);
 
+/*
 let navbarHeader = document.querySelector(".navbar-header");
 console.log(navbarHeader);
 
 let navbarFooter = document.querySelector(".navbar-footer");
 console.log(navbarFooter);
+
+let profile = document.querySelector(".profile");
+console.log(profile);
+*/
+
+let navbar = {
+    menu: document.querySelector(".amburger-menu"),
+    logo: document.querySelector(".brand-logo"),
+    header: document.querySelector(".navbar-header"),
+    footer: document.querySelector(".navbar-footer"),
+    profileInformations: document.querySelector(".profile")
+}
+
+
+navbar.menu.addEventListener("click", (event) => {
+    
+    for
+
+    for(let i = 1; i < navbar.length; i++)
+        navbar = "none";
+})
---
+++ js/script.js
@@ -0,0 +1,4 @@
+function applyProperty(htmlElement, cssPropery)
+{
+    
+}
